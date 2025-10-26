8586078f34bfc2cfa37cefa8f572fcffe16cbf2c
Alkhadi Koroma <momalkha@aol.com>
2025-10-26 14:50:42 +0000

    Voice Assistant overhaul: hide/show, drag, EN-GB/EN-US only, auto-contrast, instant mobile reflect (v202510261450)

--- js/sw-cache-buster.once.js
+++ js/sw-cache-buster.once.js
@@ -1,19 +1,10 @@
 (()=>{try{
- const VER='AUTO';
+ const VER='${VER}';
  const KEY='__mshare_swfix__';
  if(localStorage.getItem(KEY)===VER)return;
  localStorage.setItem(KEY,VER);
- if(navigator.serviceWorker){
-   navigator.serviceWorker.getRegistrations().then(rs=>{
-     rs.forEach(r=>{try{r.unregister();}catch(e){}});
-   });
- }
- if(window.caches){
-   caches.keys().then(keys=>{
-     keys.forEach(k=>{try{caches.delete(k);}catch(e){}});
-   });
- }
- const u=new URL(location.href);
- u.searchParams.set('_v',VER);
- if(location.href!==u.toString()) location.replace(u.toString());
-}catch(e){try{location.reload();}catch(_){}}})();
+ if(navigator.serviceWorker)navigator.serviceWorker.getRegistrations().then(r=>r.forEach(x=>x.unregister())));
+ if(window.caches) caches.keys().then(k=>k.forEach(x=>caches.delete(x)));
+ const u=new URL(location.href);u.searchParams.set('_v',VER);
+ location.replace(u.toString());
+}catch(e){location.reload();}})();
